a856e66ecd8aa799b5579673ebdca14e1c29e0bd
Roni Laukkarinen <roni@dude.fi>
2022-09-02 13:54:40 +0300

    Add CSS variable for color

--- colorname.sh
+++ colorname.sh
@@ -13,14 +13,18 @@ then
   CHAR4=${HEX:3:1}
   CHAR5=${HEX:4:1}
   CHAR6=${HEX:5:1}
-  HEXSPLIT=${CHAR1}${CHAR2}/${CHAR3}${CHAR4}/${CHAR5}${CHAR6}
+  HEX_SPLIT=${CHAR1}${CHAR2}/${CHAR3}${CHAR4}/${CHAR5}${CHAR6}
 
   # Scrape color name from color-name.com
   COLORNAME=$(wget -qO- "https://www.color-name.com/hex/$HEX" | hxnormalize -x | hxselect ".color-code" | lynx -stdin -dump -nolist | sed 's/     //g' | sed -e "s/Color Name: //g")
 
+  # Get CSS color code for the color name
+  COLORNAME_CSS=$(echo $COLORNAME | awk '{print tolower($0)}' | sed 's/[()]//g' | tr ' ' '-')
+  CSSVAR="--color-$COLORNAME_CSS: #$HEX;"
+
   # Print color in Terminal beautifully
   echo ""
-  printf '\e]4;1;rgb:'"$HEXSPLIT"'\e\\\e[31m██ = #'"$HEX"', '"$COLORNAME"'\e\\'
+  printf '\e]4;1;rgb:'"$HEX_SPLIT"'\e\\\e[31m██ = #'"$HEX"', '"$COLORNAME"'\e\\'
 
   # Reset colors
   # printf '\e[m'
