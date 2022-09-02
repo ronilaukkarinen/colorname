5b6d129031f054c3339b9f8559a62bb4c69dba39
Roni Laukkarinen <roni@dude.fi>
2022-09-02 13:30:59 +0300

    Add files

---
+++ README.md
@@ -0,0 +1,23 @@
+# Color name CLI
+
+This little bash program gets a color name from [color-name.com](https://www.color-name.com/).
+
+## Prequisities
+
+* macOS
+* html-xml-utils (`brew install html-xml-utils`)
+* lynx (`brew install lynx`)
+
+## Installation
+
+1. Git clone this repository or download zip and unpack it
+2. Open Terminal and cd into unpacked directory/repo
+3. `sudo mv colorname.sh /usr/local/bin/colorname && sudo chmod +x /usr/local/bin/colorname`
+
+## Usage
+
+```shell
+colorname 0049d1
+```
+
+Outputs:
---
+++ colorname.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Prequisities:
+# brew install html-xml-utils
+if [ -n "$1" ]
+then
+  # Get inputted hex color
+  HEX=$(echo "${1}" | sed 's/#//')
+
+  # Split hex in characters for printing the actual color via printf
+  CHAR1=${HEX:0:1}
+  CHAR2=${HEX:0:1}
+  CHAR3=${HEX:2:1}
+  CHAR4=${HEX:3:1}
+  CHAR5=${HEX:4:1}
+  CHAR6=${HEX:5:1}
+  HEXSPLIT=${CHAR1}${CHAR2}/${CHAR3}${CHAR4}/${CHAR5}${CHAR6}
+
+  # Scrape color name from color-name.com
+  COLORNAME=$(wget -qO- "https://www.color-name.com/hex/$HEX" | hxnormalize -x | hxselect 
+".color-code" | lynx -stdin -dump -nolist | sed 's/     //g' | sed -e "s/Color Name: //g")
+
+  # Print color in Terminal beautifully
+  echo ""
+  printf '\e]4;1;rgb:'"$HEXSPLIT"'\e\\\e[31m██ = #'"$HEX"', '"$COLORNAME"'\e\\'
+  
+  # Reset colors
+  # printf '\e[m'
+  printf '\n'
+  
+  echo ""
+
+else
+  echo 'Usage: colorname 000000 or colorname "#000000"'
+fi
